1784a2adc5c211960c9f37204895c4c45064d9af
CPD07 <peidongchen1220@gmail.com>
2025-06-27 14:04:50 +0200

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,7 @@
 /* This is the code for the classic Fibonacci number problem in Java.
 It is famous for utilising dynamic programming.
+这是经典斐波那契数列问题的 Java 代码。
+它因运用递归算法而闻名。
 */
 
 import java.util.Scanner;
