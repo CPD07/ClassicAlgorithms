8cbdfaed95239947ad28a0398b8bdd32d89c29cb
CPD07 <peidongchen1220@gmail.com>
2025-06-27 14:06:05 +0200

    Update Hanoi.py

--- Hanoi.py
+++ Hanoi.py
@@ -1,6 +1,8 @@
 """
 This is the code for the classic tower of hanoi problem in Python.
 It is famous for utilising dynamic programming.
+这是经典汉诺塔问题的 Python 代码。
+它因运用递归算法而闻名。
 """
 
 def Hanoi(n, A, B, C):
