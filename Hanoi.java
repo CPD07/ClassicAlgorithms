465cd20b21f5407e171d6b71836b066a5790bd02
CPD07 <peidongchen1220@gmail.com>
2025-06-27 14:05:45 +0200

    Update Hanoi.java

--- Hanoi.java
+++ Hanoi.java
@@ -1,5 +1,7 @@
 /* This is the code for the classic tower of hanoi problem in Java.
 It is famous for utilising dynamic programming.
+这是经典汉诺塔问题的 Java 代码。
+它因运用递归算法而闻名。
 */
 
 import java.util.Scanner;
