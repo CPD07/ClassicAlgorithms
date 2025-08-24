487c03a96c92af10a17820786874e2a2500f72e0
CPD07 <peidongchen1220@gmail.com>
2025-08-24 20:07:01 +0200

    Update aabb.py

--- aabb.py
+++ aabb.py
@@ -1,8 +1,8 @@
 """
-This is the code for the classic maths puzzle in Java.
+This is the code for the classic maths puzzle in Python.
 We use this to find square numbers that are in aabb forms.
 It is famous for utilising loop algorithms.
-这是用经典的循环代码解决数学问题的 Java 代码。
+这是用经典的循环代码解决数学问题的 Python 代码。
 这个算法可以帮我们找出aabb形式的完全平方数。
 它因运用循环代码而闻名。
 """
@@ -12,4 +12,5 @@ for a in range(1, 10):
     for b in range(0, 10):
         num = a*1100+b*11
         if int(math.sqrt(num)) == math.sqrt(num):
+
             print(num)
